bb5e0c34fe7e5eff91a454a538b86d0e9924218a
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-29 15:20:48 -0500

    Added user account functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
     def new
+        if session[:user_id]
+            redirect_to '/'
+        end
     end
   
     def create
       
-      @user = User.find_by(name: params[:user][:name])
+      @user = User.find_by(username: params[:username])
       return head(:forbidden) unless @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :orders
   resources :users
   resources :items
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
