866840961b39b4245c646456cb876fd858eda35f
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-29 12:18:52 -0500

    Add show action Change create to create session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,17 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         @user.admin = false
         if @user.save
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render 'new'
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        if @user != current_user
+            redirect_to "/items"
         end
     end
 
