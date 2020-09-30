e3d1ced75b3e920730259cf206ece382c4ead6e2
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 10:54:58 -0500

    Added validations for name

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,16 @@
 class Item < ApplicationRecord
     has_many :orders
     has_many :users, through: :orders
+    validates :name, presence: true
     validates :amount_in_stock, presence: true, numericality: {greater_than_or_equal_to: 0}
     validates :price, presence: true, numericality: {greater_than: 0}
+    validate :item_not_on_market
+    
+    private
+
+    def item_not_on_market
+        if self == Item.find_by(name: self.name)
+            errors.add(:item_not_on_market, "You're already selling that item!")
+        end
+    end
 end
