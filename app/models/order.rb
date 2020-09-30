a10d2a96c8686180511ca1b0cbf9788a415900c6
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 12:18:05 -0500

    Added validations to Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
     belongs_to :item
     belongs_to :user
+    validates :user_id, presence: true
+    validates :item_id, presence: true
+    validates :amount_id, presence: true, numericality: {greater_than: 0}
 end
