0a30bd44b9597090fe602af705e8b68ac247b842
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 09:59:54 -0500

    Added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    before_action :set_admin
+
     def current_user
         if session[:user_id]
             User.find(session[:user_id])
         end
+    end
+
+    def admin_only
+        if !@admin
+            redirect_to '/'
+        end
+    end
+
+    def set_admin
+        @admin = current_user.admin unless current_user == nil
     end
 end
