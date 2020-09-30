c03c706afe16164b5fbfaa9a574a96ec2191c75d
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 10:27:56 -0500

    fixed item validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ApplicationRecord
     has_many :orders
     has_many :users, through: :orders
     validates :amount_in_stock, presence: true, numericality: {greater_than_or_equal_to: 0}
-    validates :price, presence: true, numericality: {greater_than_or_equal_to: 0}
+    validates :price, presence: true, numericality: {greater_than: 0}
 end
