ab10388086fdc6967ce1e963e6a55e051d13fdc3
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 11:55:20 -0500

    Fixed custom validation more

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
     private
 
     def item_not_on_market
-        if Item.find_by(name: self.name)
+        if Item.find_by(name: self.name) && !self.id
             errors.add(:you, "are already selling that item!")
             #Message is added at the key `:you` solely to create full message: 
             #"You are already selling that item!"
