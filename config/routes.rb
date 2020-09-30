5e06d3c8580fc8a7c67554536fe21b54268b1b07
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 10:01:25 -0500

    added routing for toggling admin permissions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :items
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#new'
+  get '/toggle/:id', to: 'users#show_toggle'
+  post '/toggle/:id', to: 'users#toggle'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
