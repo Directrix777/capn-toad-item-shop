87fe0443539228df0279f329f32cf80a1635f4af
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 15:26:49 -0500

    Randomly generate a nice secure password for our lovely fb folk

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: true, presence: true
     validates :password_digest, presence: true
+
+    def self.randomly_generate
+        (0...25).map { (65 + rand(26)).chr }.join
+    end
 end
