5fef72706deb9df4dc22b02fa8e8028cdb5dc7af
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 10:08:08 -0500

    Added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :orders
     has_many :items, through: :orders
     has_secure_password
+    validates :username, uniquness: true, presence: true
+    validates :password_digest, presence: true
 end
