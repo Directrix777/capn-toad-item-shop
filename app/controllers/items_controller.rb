eed076b1678047069aa2c3148e01f0d449180abe
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 10:33:42 -0500

    added basic CRUD actions to Items controler

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,47 @@
 class ItemsController < ApplicationController
+    def index
+        @items = Item.all
+    end
+
+    def show
+        @item = Item.find(params[:id])
+    end
+
+    def new
+        admin_only
+        @item = Item.new
+    end
+
+    def create
+        admin_only
+        @item = Item.new(item_params)
+        if @item.save
+            redirect_to item_path(item)
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        admin_only
+        @item = Item.find(params[:id])
+    end
+
+    def update
+        admin_only
+        @item = Item.find(params[:id])
+        if @item.update(item_params)
+            redirect_to item_path(@item)
+        else
+            render 'edit'
+        end
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:amount_in_stock, :price)
+    end
+
+
 end
