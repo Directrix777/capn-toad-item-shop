83b90859fb8bdebdaf32d7198a18998aad96d17b
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 13:37:23 -0500

    Built controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,39 @@
 class OrdersController < ApplicationController
+    def new
+        @order = Order.new
+        @item = Item.find(params[:item_id])
+    end
+
+    def create
+        @order = Order.new(order_params)
+        if @order.save
+            @order.send_order
+            redirect_to '/'
+        else
+            @item = Item.find(params[:item_id])
+            render 'new'
+        end
+    end
+
+    def index
+        admin_only
+        @orders = Order.all
+        if params[:id]
+            @orders = @orders.where(item_id: params[:item_id])
+        end
+    end
+
+    def destroy
+        admin_only
+        @item = Item.find(params[:id])
+        @item.delete
+        redirect_to items_path
+    end
+
+    private
+
+    def order_params
+        params.require(:order).permit(:amount, :user_id, :item_id)
+    end
+
 end
