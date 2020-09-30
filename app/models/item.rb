d0db79e099ab28173e91536a7c6c866922e1c9e5
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 10:24:47 -0500

    Added validations to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
     has_many :orders
     has_many :users, through: :orders
+    validates :amount_in_stock, presence: true, numericality: {greater_than_or_equal_to: 0}
+    validates :price, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
