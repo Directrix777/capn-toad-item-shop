1dcadd0663d792aae1aff26d32426955a0fa2002
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-30 12:28:26 -0500

    Adjutsing routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :items
   resources :items do
-    resources :orders, only: [:new, :create, :index :delete]
+    resources :orders, only: [:new, :create, :index, :delete]
   end
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#new'
